0602fc384ccdec9ca6c725333e40ff94e2e0266d
João Freitas <joao.mag.freitas@gmail.com>
2026-08-20 17:38:11 +0100

    [usecases] - get launcher app list (#8)

--- app/src/main/java/link/joaomagfreitas/minimal_launcher/repositories/DeviceRepository.kt
+++ app/src/main/java/link/joaomagfreitas/minimal_launcher/repositories/DeviceRepository.kt
@@ -6,28 +6,30 @@ import android.content.Intent
 import link.joaomagfreitas.minimal_launcher.models.DeviceAppModel
 
 interface DeviceRepository {
-    suspend fun queryApps(): List<DeviceAppModel>
+    suspend fun queryApps(): Result<List<DeviceAppModel>>
     suspend fun openApp(app: DeviceAppModel)
 }
 
 class IpcDeviceRepository(
     private val context: Context,
 ) : DeviceRepository {
-    override suspend fun queryApps(): List<DeviceAppModel> {
-        val intent = Intent(Intent.ACTION_MAIN).apply {
-            addCategory(Intent.CATEGORY_LAUNCHER)
-        }
-
-        return context
-            .packageManager
-            .queryIntentActivities(intent, 0)
-            .map { info ->
-                DeviceAppModel(
-                    label = info.loadLabel(context.packageManager).toString(),
-                    packageName = info.activityInfo.packageName,
-                    activityName = info.activityInfo.name,
-                )
+    override suspend fun queryApps(): Result<List<DeviceAppModel>> {
+        return runCatching {
+            val intent = Intent(Intent.ACTION_MAIN).apply {
+                addCategory(Intent.CATEGORY_LAUNCHER)
             }
+
+            return@runCatching context
+                .packageManager
+                .queryIntentActivities(intent, 0)
+                .map { info ->
+                    DeviceAppModel(
+                        label = info.loadLabel(context.packageManager).toString(),
+                        packageName = info.activityInfo.packageName,
+                        activityName = info.activityInfo.name,
+                    )
+                }
+        }
     }
 
     override suspend fun openApp(app: DeviceAppModel) {
--- app/src/main/java/link/joaomagfreitas/minimal_launcher/repositories/LauncherRepository.kt
+++ app/src/main/java/link/joaomagfreitas/minimal_launcher/repositories/LauncherRepository.kt
@@ -8,17 +8,19 @@ import link.joaomagfreitas.minimal_launcher.models.LauncherAppListItemModel
 import java.io.File
 
 interface LauncherRepository {
-    suspend fun getAppList(): List<LauncherAppListItemModel>
+    suspend fun getAppList(): Result<List<LauncherAppListItemModel>>
     suspend fun updateAppList(items: List<LauncherAppListItemModel>)
 }
 
 class InternalStorageLauncherRepository(
     private val context: Context,
 ) : LauncherRepository {
-    override suspend fun getAppList(): List<LauncherAppListItemModel> {
-        return withContext(Dispatchers.IO) {
-            val file = appsListJsonFile
-            Json.decodeFromString(file.readText())
+    override suspend fun getAppList(): Result<List<LauncherAppListItemModel>> {
+        return runCatching {
+            withContext(Dispatchers.IO) {
+                val file = appsListJsonFile
+                Json.decodeFromString(file.readText())
+            }
         }
     }
 
