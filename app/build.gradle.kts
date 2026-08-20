e307d3cfad45b5619886b2ff1905dde4b01a341c
João Freitas <joao.mag.freitas@gmail.com>
2026-08-20 17:20:27 +0100

    [usecases] - update launcher app list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -48,6 +49,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.material)
     implementation(libs.reorderable)
+    implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
--- app/src/main/java/link/joaomagfreitas/minimal_launcher/models/DeviceAppModel.kt
+++ app/src/main/java/link/joaomagfreitas/minimal_launcher/models/DeviceAppModel.kt
@@ -1,5 +1,8 @@
 package link.joaomagfreitas.minimal_launcher.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class DeviceAppModel(
     val label: String,
     val packageName: String,
