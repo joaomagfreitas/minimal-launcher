812710932ddf35cc1ed9cb899802b2d5d0a6bafa
João Freitas <joao.mag.freitas@gmail.com>
2026-08-20 10:21:42 +0100

    [design] - import generated sources from stitch (#1)

--- app/src/main/java/link/joaomagfreitas/minimal_launcher/ui/theme/Color.kt
+++ app/src/main/java/link/joaomagfreitas/minimal_launcher/ui/theme/Color.kt
@@ -1,11 +1,17 @@
 package link.joaomagfreitas.minimal_launcher.ui.theme
 
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/* Mono-Focus color tokens */
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primary = Color(0xFFFFFFFF)
+val secondary = Color(0xFF1A1A1A)
+val tertiary = Color(0xFFFFFFFF)
+val neutral = Color(0xFF808080)
+
+val monoFocusDarkColorScheme = darkColorScheme(
+    primary = primary,
+    secondary = secondary,
+    tertiary = tertiary,
+)
