23d3ed11f2796bded00491d8e9d1b990f8716870
João Freitas <joao.mag.freitas@gmail.com>
2026-08-20 09:37:59 +0100

    [chore] - init project

---
+++ app/src/main/java/link/joaomagfreitas/minimal_launcher/activities/LauncherActivity.kt
@@ -0,0 +1,10 @@
+package link.joaomagfreitas.minimal_launcher.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class LauncherActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
