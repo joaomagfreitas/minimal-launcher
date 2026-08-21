30fa95bb9bb46079d3985d7ba2fec32ea70a4322
João Freitas <joao.mag.freitas@gmail.com>
2026-08-21 23:57:17 +0100

    [chore] - setup lint and format

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.ktlint)
 }
 
 android {
