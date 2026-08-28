02dd8e81268f2d55bd74e5b8abb9d27af9fcb5b3
João Freitas <joao.mag.freitas@gmail.com>
2026-08-28 17:13:13 +0100

    [window] - set system navigation bar (2/3 button) transparent (#16)

---
+++ app/src/main/java/link/joaomagfreitas/minimal_launcher/ui/wrappers/WindowWrapper.kt
@@ -0,0 +1,10 @@
+package link.joaomagfreitas.minimal_launcher.ui.wrappers
+
+import android.os.Build
+import android.view.Window
+
+fun Window.setTransparentSystemNavigationBar() {
+  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+    isNavigationBarContrastEnforced = false
+  }
+}
