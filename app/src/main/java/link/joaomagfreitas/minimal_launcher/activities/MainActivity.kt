84f8fd19afbe46bea4920d29ebd4cf6879874b57
João Freitas <joao.mag.freitas@gmail.com>
2026-08-28 17:07:31 +0100

    [window] - set system navigation bar (2/3 button) transparent

--- app/src/main/java/link/joaomagfreitas/minimal_launcher/activities/MainActivity.kt
+++ app/src/main/java/link/joaomagfreitas/minimal_launcher/activities/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import link.joaomagfreitas.minimal_launcher.ui.screens.LauncherScreen
+import link.joaomagfreitas.minimal_launcher.ui.wrappers.setTransparentSystemNavigationBar
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
+    window.setTransparentSystemNavigationBar()
 
     setContent { LauncherScreen() }
   }
