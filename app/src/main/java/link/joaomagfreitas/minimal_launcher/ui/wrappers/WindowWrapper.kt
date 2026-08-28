dc8654bde8241c220e1011d02e9e20b193e2a99a
João Freitas <joao.mag.freitas@gmail.com>
2026-08-28 17:10:31 +0100

    [window] - tidy code

--- app/src/main/java/link/joaomagfreitas/minimal_launcher/ui/wrappers/WindowWrapper.kt
+++ app/src/main/java/link/joaomagfreitas/minimal_launcher/ui/wrappers/WindowWrapper.kt
@@ -4,7 +4,7 @@ import android.os.Build
 import android.view.Window
 
 fun Window.setTransparentSystemNavigationBar() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        isNavigationBarContrastEnforced = false
-    }
+  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+    isNavigationBarContrastEnforced = false
+  }
 }
